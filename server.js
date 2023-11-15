558332c2cbe90ba14c9e9d8ea9da79a7d05ba39c
Duarte Nunes <duartenunes4ever@gmail.com>
2023-11-15 17:51:13 +0000

    things

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const dbConfig = require("./app/config/db.config");
 
 const app = express();
 
-var corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200 // For legacy browser support
-};
+// Enable CORS for all origins
+app.use(cors());
 
-app.use(cors(corsOptions));
+// Additional middleware to log the response headers
+// This is for debugging purposes and should be removed or adjusted for production
+app.use((req, res, next) => {
+  const defaultWrite = res.write;
+  const defaultEnd = res.end;
+  const chunks = [];
+  res.write = function (chunk) {
+    chunks.push(chunk);
+    return defaultWrite.apply(res, arguments);
+  };
+  res.end = function (chunk) {
+    if (chunk) chunks.push(chunk);
+    const body = Buffer.concat(chunks).toString('utf8');
+    console.log(req.path, body); // Log the body of the response
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    defaultEnd.apply(res, arguments);
+  };
+  next();
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -17,9 +33,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Enable preflight requests for all routes
+app.options('*', cors());
+
 const db = require("./app/models");
 const Role = db.role;
 
+// Connect to MongoDB
 db.mongoose
   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
@@ -34,51 +54,48 @@ db.mongoose
     process.exit();
   });
 
-// simple route
+// Simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-// routes
+// Routes
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
-// set port, listen for requests
+// Set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+// Function to initialize roles in the database
 function initial() {
   Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
+    if (err) {
+      console.error("Role initialization error", err);
+      return;
+    }
+    
+    if (count === 0) {
+      new Role({ name: "user" }).save(err => {
         if (err) {
-          console.log("error", err);
+          console.error("error", err);
         }
-
         console.log("added 'user' to roles collection");
       });
 
-      new Role({
-        name: "moderator"
-      }).save(err => {
+      new Role({ name: "moderator" }).save(err => {
         if (err) {
-          console.log("error", err);
+          console.error("error", err);
         }
-
         console.log("added 'moderator' to roles collection");
       });
 
-      new Role({
-        name: "admin"
-      }).save(err => {
+      new Role({ name: "admin" }).save(err => {
         if (err) {
-          console.log("error", err);
+          console.error("error", err);
         }
-
         console.log("added 'admin' to roles collection");
       });
     }
